49e099ce2643e89e6314107c3fcf6777a91c70fa
DucTran2310 <trananhduc23102000@gmail.com>
2023-05-04 01:38:50 +0700

    Initial project airbnb_clone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import { Nunito } from 'next/font/google'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+const font = Nunito({
+  subsets: ['latin']
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={font.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
