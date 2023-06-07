2a01b23bc15e3b40dfbc26bc611e9284a2e4fd85
DucTran2310 <trananhduc23102000@gmail.com>
2023-06-07 22:33:06 +0700

    Add UI counter component

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,7 +1,8 @@
-'use client'
+'use client';
 
 import { useCallback, useEffect, useState } from "react";
-import { IoMdClose } from 'react-icons/io'
+import { IoMdClose } from "react-icons/io";
+
 import Button from "../Button";
 
 interface ModalProps {
@@ -14,7 +15,7 @@ interface ModalProps {
     actionLabel: string;
     disabled?: boolean;
     secondaryAction?: () => void;
-    secondaryLabel?: string
+    secondaryActionLabel?: string;
 }
 
 const Modal: React.FC<ModalProps> = ({
@@ -23,132 +24,125 @@ const Modal: React.FC<ModalProps> = ({
     onSubmit,
     title,
     body,
-    footer,
     actionLabel,
+    footer,
     disabled,
     secondaryAction,
-    secondaryLabel
+    secondaryActionLabel
 }) => {
-
-    const [showModal, setShowModal] = useState(isOpen)
+    const [showModal, setShowModal] = useState(isOpen);
 
     useEffect(() => {
-        setShowModal(isOpen)
-    }, [isOpen])
+        setShowModal(isOpen);
+    }, [isOpen]);
 
     const handleClose = useCallback(() => {
         if (disabled) {
-            return
+            return;
         }
 
-        setShowModal(false)
+        setShowModal(false);
         setTimeout(() => {
-            onClose()
+            onClose();
         }, 300)
-    }, [disabled, onClose])
+    }, [onClose, disabled]);
 
     const handleSubmit = useCallback(() => {
         if (disabled) {
-            return
+            return;
         }
 
-        onSubmit()
-    }, [disabled, onSubmit])
+        onSubmit();
+    }, [onSubmit, disabled]);
 
     const handleSecondaryAction = useCallback(() => {
         if (disabled || !secondaryAction) {
-            return
+            return;
         }
 
-        secondaryAction()
-    }, [disabled, secondaryAction])
+        secondaryAction();
+    }, [secondaryAction, disabled]);
 
     if (!isOpen) {
-        return null
+        return null;
     }
 
     return (
         <>
             <div
                 className="
-                    justify-center
-                    items-center
-                    flex
-                    overflow-x-hidden
-                    overflow-y-auto
-                    fixed
-                    inset-0
-                    z-50
-                    outline-none
+                    justify-center 
+                    items-center 
+                    flex 
+                    overflow-x-hidden 
+                    overflow-y-auto 
+                    fixed 
+                    inset-0 
+                    z-50 
+                    outline-none 
                     focus:outline-none
                     bg-neutral-800/70
                 "
             >
-                <div
-                    className="
-                        relative
+                <div className="
+                        relative 
                         w-full
                         md:w-4/6
                         lg:w-3/6
                         xl:w-2/5
-                        my-2
-                        mx-auto
-                        h-full
+                        my-6
+                        mx-auto 
+                        h-full 
                         lg:h-auto
                         md:h-auto
                     "
                 >
-                    {/* CONTENT */}
-                    <div
-                        className={`
-                            tranlate
-                            duration-300
-                            h-full
-                            ${showModal ? 'translate-y-0' : 'translate-y-full'}
-                            ${showModal ? 'opacity-100' : 'opacity-0'}
-                        `}
-                    >
-                        <div
-                            className="
+                    {/*content*/}
+                    <div className={`
+                        translate
+                        duration-300
+                        h-full
+                        ${showModal ? 'translate-y-0' : 'translate-y-full'}
+                        ${showModal ? 'opacity-100' : 'opacity-0'}
+                    `}>
+                        <div className="
                                 translate
                                 h-full
                                 lg:h-auto
-                                mt-[10%]
                                 md:h-auto
-                                border-0
-                                rounded-lg
-                                shadow-lg
-                                relative
-                                flex
-                                flex-col
-                                w-full
-                                bg-white
-                                outline-none
+                                border-0 
+                                rounded-lg 
+                                shadow-lg 
+                                relative 
+                                flex 
+                                flex-col 
+                                w-full 
+                                bg-white 
+                                outline-none 
                                 focus:outline-none
                             "
                         >
-                            {/* HEADER */}
-                            <div
-                                className="
-                                   flex
-                                   items-center
-                                   pt-1.5
-                                   rounded-t
-                                   justify-center
-                                   relative
-                                   border-b-[1px] 
+                            {/*header*/}
+                            <div className="
+                                    flex 
+                                    items-center 
+                                    p-6
+                                    rounded-t
+                                    justify-center
+                                    relative
+                                    border-b-[1px]
                                 "
                             >
                                 <button
-                                    onClick={handleClose}
                                     className="
                                         p-1
-                                        border-0
+                                        border-0 
                                         hover:opacity-70
                                         transition
                                         absolute
                                         left-9
                                     "
+                                    onClick={handleClose}
                                 >
                                     <IoMdClose size={18} />
                                 </button>
@@ -156,30 +150,29 @@ const Modal: React.FC<ModalProps> = ({
                                     {title}
                                 </div>
                             </div>
-                            {/* BODY */}
-                            <div className="relative p-6 pb-2 flex-auto">
+                            {/*body*/}
+                            <div className="relative p-6 flex-auto">
                                 {body}
                             </div>
-                            {/* FOOTER */}
-                            <div className="flex flex-col gap-1 p-6">
+                            {/*footer*/}
+                            <div className="flex flex-col gap-2 p-6">
                                 <div
                                     className="
-                                        flex
-                                        flex-row
-                                        items-center
-                                        gap-4
+                                        flex 
+                                        flex-row 
+                                        items-center 
+                                        gap-4 
                                         w-full
                                     "
                                 >
-                                    {
-                                        secondaryAction && secondaryLabel && (
-                                            <Button
-                                                disabled={disabled}
-                                                label={secondaryLabel}
-                                                onClick={handleSecondaryAction}
-                                            />
-                                        )
-                                    }
+                                    {secondaryAction && secondaryActionLabel && (
+                                        <Button
+                                            disabled={disabled}
+                                            label={secondaryActionLabel}
+                                            onClick={handleSecondaryAction}
+                                            outline
+                                        />
+                                    )}
                                     <Button
                                         disabled={disabled}
                                         label={actionLabel}
